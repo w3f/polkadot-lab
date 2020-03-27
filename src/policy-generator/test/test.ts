0afa29656312586f9ee2e1c831b071a713eb883d
pantelis <pampatzoglou@gmail.com>
2020-03-27 19:19:51 +0200

    review

--- src/policy-generator/test/test.ts
+++ src/policy-generator/test/test.ts
@@ -3,25 +3,17 @@ const YAML = require('js-yaml');
 const fs = require('fs');
 const NetworkPolicy = require('../policies_generator.ts');
 
-let test_10_circle = new NetworkPolicy(10);
-let test_10_line = new NetworkPolicy(10, "line");
-let test_10_full = new NetworkPolicy(10, "full");
-
 try {
     let data1 = fs.readFileSync('./10-circle-network-policie.example', 'utf8');
+    let test_10_circle = new NetworkPolicy(10);
     assert.equal(test_10_circle.getPolicy(), data1, "Failed circle policie");
-} catch (e) {
-    console.log(e);
-}
 
-try {
     let data2 = fs.readFileSync('./10-line-network-policie.example', 'utf8');
+    let test_10_line = new NetworkPolicy(10, "line");
     assert.equal(test_10_line.getPolicy(), data2, "Failed line policie");
-} catch (e) {
-    console.log(e);
-}
-try {
+
     let data3 = fs.readFileSync('./10-full-network-policie.example', 'utf8');
+    let test_10_full = new NetworkPolicy(10, "full");
     assert.equal(test_10_full.getPolicy(), data3, "Failed full policie");
 } catch (e) {
     console.log(e);
