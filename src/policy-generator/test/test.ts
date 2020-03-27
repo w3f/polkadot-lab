6fde8e5d2e619a85c550d513f7783e25c9c04f90
pantelis <pampatzoglou@gmail.com>
2020-03-27 19:07:01 +0200

    review

--- src/policy-generator/test/test.ts
+++ src/policy-generator/test/test.ts
@@ -3,9 +3,9 @@ const YAML = require('js-yaml');
 const fs = require('fs');
 const NetworkPolicy = require('../policies_generator.ts');
 
-test_10_circle = new NetworkPolicy(10);
-test_10_line = new NetworkPolicy(10, "line");
-test_10_full = new NetworkPolicy(10, "full");
+let test_10_circle = new NetworkPolicy(10);
+let test_10_line = new NetworkPolicy(10, "line");
+let test_10_full = new NetworkPolicy(10, "full");
 
 try {
     let data1 = fs.readFileSync('./10-circle-network-policie.example', 'utf8');
