b9ba4fbac7621f77de9e29f7c28faee4f36a0253
Federico Gimenez <federico.gimenez@gmail.com>
2020-03-27 19:48:49 +0100

    instantiate engine from start action

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -1,7 +1,26 @@
-import { createLogger } from '../logger';
+import readline from 'readline';
 
-export function startAction(): void {
+//import { Config } from '../config';
+import { createLogger } from '../logger';
+import { Engine } from '../engine';
+
+export async function startAction(): Promise<void> {
+    //const cfg = Config.parse(cmd.config);
+
     const logger = createLogger('info');
 
-    logger.info('Hello world!')
+    // TODO: instantiate result service and clients
+
+    const engine = new Engine(logger);
+
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', async (str, key) => {
+        if (key.ctrl && key.name === 'c') {
+            await engine.stop();
+            process.exit();
+        }
+    });
+
+    await engine.start();
 }
