828642d5a5513e628abd1a8139f03f8abf11554c
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-28 15:02:49 +0200

    cleanup

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -4,24 +4,15 @@ import { createLogger } from '@w3f/logger';
 import { InputConfig } from '../types';
 import { Engine } from '../engine';
 
-export async function startAction(): Promise<void> {
-  try {
-      const cfg = new Config<InputConfig>().parse(cmd.config);
-      const logger = createLogger(cfg.logLevel);
-      const engine = new Engine(cfg.items, logger);
+export async function startAction(cmd): Promise<void> {
+    const cfg = new Config<InputConfig>().parse(cmd.config);
+    const logger = createLogger(cfg.logLevel);
+    const engine = new Engine(cfg.items, logger);
 
-      // TODO: instantiate result service and clients
-      readline.emitKeypressEvents(process.stdin);
-      process.stdin.setRawMode(true);
-      process.stdin.on('keypress', async (str, key) => {
-          if (key.ctrl && key.name === 'c') {
-              await engine.stop();
-              process.exit();
-          }
-      });
-      await engine.start();
-  } catch (e) {
-      console.log(e);
-      process.exit(1);
-  }
+    try {
+        await engine.start();
+    } catch (e) {
+        logger.error(e);
+        process.exit(1);
+    }
 }
