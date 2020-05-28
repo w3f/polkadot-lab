8d328e669d0cee4878b645da156092b8eb24c7a3
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-28 15:52:11 +0200

    use common Topology type

--- test/network-policy-generator/network-policy-generator.ts
+++ test/network-policy-generator/network-policy-generator.ts
@@ -1,12 +1,14 @@
-import { Policy, Topology } from '../../src/network-policy-generator';
 import { should } from 'chai';
+
+import { Policy } from '../../src/network-policy-generator';
+import { Topology } from '../../src/types';
 
 should();
 
 describe('Policy Generator', () => {
     it('should read valid yaml', () => {
-      const test = new Policy(10, Topology.Line);
-      const config = test.generate();
-      config.should.exist;
+        const test = new Policy(10, Topology.Line);
+        const config = test.generate();
+        config.should.exist;
     });
 });
