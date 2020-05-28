f38b63f12f2a6f2b299dff1caff0bb7b12e46603
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-28 15:36:09 +0200

    engine config and cleanup

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,27 @@
-export interface Logger {
-    info: (msg: string) => void;
-}
-
 export enum Topology {
-    Line = "Line",
-    Full = "Full"
+    Line = 'Line',
+    Full = 'Full'
 }
 
-export interface InputConfig {
-    logLevel: string;
-    maximumExecutionTime: string;
+export enum Metric {
+    TimeToFinality = 'TimeToFinality'
+}
+
+export type Metrics = Array<Metric>;
+
+export interface EngineManager {
+    start(): Promise<void>;
+    stop(): Promise<void>;
+}
+
+export interface EngineConfig {
     nodes: number;
     topology: Topology;
     targetStd: number;
+    metrics: Metrics;
+}
+
+export interface InputConfig extends EngineConfig {
+    logLevel: string;
+    maximumExecutionTime: string;
 }
