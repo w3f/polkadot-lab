7be734014c073d5de77ece3376a434aaf3f353c3
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-28 18:15:14 +0200

    logic skeleton and required types

--- src/engine.ts
+++ src/engine.ts
@@ -1,12 +1,50 @@
 import { Logger } from '@w3f/logger';
 
-import { EngineManager, EngineConfig } from './types';
+import {
+    ApplicationsManager,
+    EngineManager,
+    EngineConfig,
+    PlatformManager,
+    ResultsManager
+} from './types';
 
 export class Engine implements EngineManager {
-    constructor(private readonly cfg: EngineConfig, private readonly logger: Logger) { }
+    private platform: PlatformManager;
+    private apps: ApplicationsManager;
+    private results: ResultsManager;
+    private logger: Logger;
+
+    constructor(cfg: EngineConfig) {
+        this.platform = cfg.platform;
+        this.apps = cfg.apps;
+        this.results = cfg.results;
+    }
 
     async start(): Promise<void> {
         this.logger.info('Execution engine started');
+
+        this.logger.info('Creating platform...');
+        await this.platform.create();
+        const kubeconfig = await this.platform.getKubeconfig();
+        this.logger.info('Platform created');
+
+        this.apps.setKubeconfig(kubeconfig);
+
+        this.logger.info('Installing dependencies...');
+        await this.apps.installDependencies();
+        this.logger.info('Dependencies installed');
+
+        this.logger.info('Installing nodes...');
+        await this.apps.installNodes();
+        this.logger.info('Nodes installed');
+
+        this.logger.info('Executing tests...');
+        await this.results.runTests();
+        this.logger.info('Tests executed');
+
+        this.logger.info('Writing results...');
+        await this.results.writeResults();
+        this.logger.info('Results written');
     }
 
     async stop(): Promise<void> {
--- src/types.ts
+++ src/types.ts
@@ -14,14 +14,37 @@ export interface EngineManager {
     stop(): Promise<void>;
 }
 
-export interface EngineConfig {
+export interface PlatformManager {
+    create(): Promise<void>;
+    destroy(): Promise<void>;
+    getKubeconfig(): Promise<string>;
+}
+
+export interface ApplicationsManager {
+    setKubeconfig(kubeconfig: string): void;
+    installDependencies(): Promise<void>;
+    installNodes(): Promise<void>;
+}
+
+export interface ResultsManager {
+    runTests(): Promise<void>;
+    writeResults(): Promise<void>;
+}
+
+export interface LabConfig {
     nodes: number;
     topology: Topology;
     targetStd: number;
     metrics: Metrics;
 }
 
-export interface InputConfig extends EngineConfig {
+export interface InputConfig extends LabConfig {
     logLevel: string;
     maximumExecutionTime: string;
+}
+
+export interface EngineConfig extends LabConfig {
+    platform: PlatformManager;
+    apps: ApplicationsManager;
+    results: ResultsManager;
 }
