76cc4ec65973bd0da8fe8a7103ce385140ed0e31
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-29 11:16:07 +0200

    apps husk

---
+++ src/apps/index.ts
@@ -0,0 +1,13 @@
+import { Logger } from '@w3f/logger';
+
+import { Topology, ApplicationsManager } from '../types';
+
+
+export class Apps implements ApplicationsManager {
+    constructor(
+        private readonly topology: Topology,
+        private readonly logger: Logger
+    ) { }
+
+    async install(kubeconfig: string): Promise<void> { }
+}
