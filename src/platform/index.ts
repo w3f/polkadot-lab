141f83f39958c78dbbb0e68a45e7173b73129919
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-29 15:19:20 +0200

    main platform class as proxy to the different strategies

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -1,21 +1,29 @@
 import { Logger } from '@w3f/logger';
 
 import { ExecutionMode, PlatformManager } from '../types';
+import { LocalPlatform } from './local';
 
 
 export class Platform implements PlatformManager {
+    private readonly strategy: PlatformManager;
+
     constructor(
         private readonly mode: ExecutionMode,
         private readonly nodes: number,
-        private readonly logger: Logger) { }
+        private readonly logger: Logger
+    ) {
+        if (mode === ExecutionMode.Local) {
+            this.strategy = new LocalPlatform(nodes, logger);
+        }
+    }
 
     async create(): Promise<void> {
-        return
+        return this.strategy.create();
     }
     async destroy(): Promise<void> {
-        return
+        return this.strategy.destroy();
     }
     async getKubeconfig(): Promise<string> {
-        return
+        return this.strategy.getKubeconfig();
     }
 }
