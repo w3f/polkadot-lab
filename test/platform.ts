d465719e5c90ed343872fbbe8b809aa67cced37a
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-29 13:31:17 +0200

    added unit test

---
+++ test/platform.ts
@@ -0,0 +1,20 @@
+import { should } from 'chai';
+import { createLogger } from '@w3f/logger';
+
+import { Platform } from '../src/platform';
+import { ExecutionMode } from '../src/types';
+
+should();
+
+const logger = createLogger();
+
+describe('Platform', () => {
+    it('should be instantiable', () => {
+        const mode = ExecutionMode.Local;
+        const nodes = 4;
+
+        const platform = new Platform(mode, nodes, logger);
+
+        platform.should.exist;
+    });
+});
