e7f35967408239359ca3bf6a96422229035dd1dd
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-29 11:16:54 +0200

    instantiate and inject apps and results

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -3,6 +3,9 @@ import { createLogger } from '@w3f/logger';
 
 import { InputConfig } from '../types';
 import { Engine } from '../engine';
+import { Platform } from '../platform';
+import { Apps } from '../apps';
+import { Results } from '../results';
 
 
 export async function startAction(cmd): Promise<void> {
@@ -10,14 +13,17 @@ export async function startAction(cmd): Promise<void> {
     const logger = createLogger(cfg.logLevel);
 
     const platform = new Platform(cfg.mode, cfg.nodes, logger);
+    const apps = new Apps(cfg.topology, logger);
+    const results = new Results(cfg.targetStd, cfg.metrics, logger);
 
     const engineCfg = {
         platform,
+        apps,
+        results,
         logger
     }
 
     const engine = new Engine(engineCfg);
-
 
     try {
         await engine.start();
