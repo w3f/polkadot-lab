99b915bd5141fe3a12a20fcd018b7fc08d187277
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-05 17:47:15 +0200

    apps: logic and install prometheus-operator

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,15 +1,66 @@
 import { Logger } from '@w3f/logger';
+import { Helm, ChartConfig } from '@w3f/helm';
+import path from 'path';
 
-import { Topology, ApplicationsManager } from '../types';
+import {
+    Topology,
+    ApplicationsManager,
+    Dependencies
+} from '../types';
 
 
 export class Apps implements ApplicationsManager {
+    private helm: Helm;
+
     constructor(
         private readonly topology: Topology,
+        private readonly dependencies: Dependencies,
         private readonly logger: Logger
     ) { }
 
     async install(kubeconfig: string): Promise<void> {
-        return
+        await this.init(kubeconfig);
+
+        await this.installDependencies();
+
+        await this.installNodes();
+    }
+
+    private async init(kubeconfig: string): Promise<void> {
+        if (this.helm) {
+            return
+        }
+        this.helm = await Helm.create(kubeconfig, this.logger);
+    }
+
+    private async installDependencies(): Promise<void> {
+        await this.installPrometheus();
+
+        // install network-policy
+    }
+
+    private async installNodes(): Promise<void> {
+        // install polkadot-base-services
+
+        // install polkadot
+    }
+
+    private async installPrometheus(): Promise<void> {
+        const valuesTemplatePath = path.join(__dirname, 'values', 'prometheus-operator.yaml');
+        const valuesTemplate = {
+            path: valuesTemplatePath,
+            data: {}
+        }
+        const chartCfg: ChartConfig = {
+            name: 'prometheus-operator',
+            chart: 'stable/prometheus-operator',
+            wait: true,
+            valuesTemplate
+        };
+        if (this.dependencies &&
+            this.dependencies['prometheus-operator']) {
+            chartCfg.version = this.dependencies['prometheus-operator'];
+        }
+        await this.helm.install(chartCfg);
     }
 }
