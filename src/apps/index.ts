1fb604e7a8417092645cf9fa8ea9c0b3cfdca47a
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-11 14:11:44 +0200

    use chart managers

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,18 +1,22 @@
 import { Logger } from '@w3f/logger';
-import { Helm, ChartConfig } from '@w3f/helm';
-import { Crypto, KeyTypes, KeysBundle } from '@w3f/crypto';
+import { Helm } from '@w3f/helm';
 import path from 'path';
 
 import {
     Topology,
     ApplicationsManager,
-    Dependencies
+    Dependencies,
+    ChartManager
 } from '../types';
-
+import {
+    NetworkPolicyChart,
+    PolkadotBaseServicesChart,
+    PolkadotChart,
+    PrometheusOperatorChart
+} from './charts';
 
 export class Apps implements ApplicationsManager {
     private helm: Helm;
-    private crypto: Crypto;
 
     constructor(
         private readonly topology: Topology,
@@ -43,104 +47,46 @@ export class Apps implements ApplicationsManager {
                 }
             ]);
         }
-        if (!this.crypto) {
-            this.crypto = new Crypto(this.size);
-        }
     }
 
     private async installDependencies(): Promise<void> {
-        await this.installPrometheus();
-
-        await this.installNetworkPolicy();
-    }
-
-    private async installNodes(): Promise<void> {
-        await this.installPolkadotBaseServices();
-
-        const keys = await this.crypto.createKeys();
-        for (let i = 0; i < this.size; i++) {
-            await this.installPolkadot(i, keys);
+        const chartTypes = [PrometheusOperatorChart, NetworkPolicyChart, PolkadotBaseServicesChart];
+        for (let chartType of chartTypes) {
+            const chart = new chartType(this.topology, this.size, this.dependencies, this.logger);
+            await this.installChart(chart);
         }
     }
 
-    private async installPrometheus(): Promise<void> {
-        const chartCfg: ChartConfig = {
-            name: 'prometheus-operator',
-            chart: 'stable/prometheus-operator',
-            wait: true
-        };
-        await this.installChart(chartCfg);
+    private async installNodes(): Promise<void> {
+        const chart = new PolkadotChart(this.topology, this.size, this.dependencies, this.logger);
+        for (let i = 0; i < this.size; i++) {
+            await this.installChart(chart);
+        }
     }
 
-    private async installNetworkPolicy(): Promise<void> {
-        const data = {
-            topology: this.topology,
-            size: this.size
-        };
-        const chartCfg: ChartConfig = {
-            name: 'network-policy',
-            chart: 'w3f/network-policy',
-            wait: true
-        };
-        await this.installChart(chartCfg, data);
-    }
+    private async installChart(chart: ChartManager): Promise<void> {
+        const chartCfg = await chart.cfg();
+        const data = await chart.data();
 
-    private async installPolkadotBaseServices(): Promise<void> {
-        const chartCfg: ChartConfig = {
-            name: 'polkadot-base-services',
-            chart: 'w3f/polkadot-base-services',
-            wait: true
-        };
-        await this.installChart(chartCfg);
-    }
-
-    private async installPolkadot(order: number, keys: KeysBundle): Promise<void> {
-        const data = {
-            name: `polkadot-${order}`,
-            deploymentName: 'polkadot-lab',
-
-            chainspecStashAddresses: keys[KeyTypes.Stash].map(item => item.address),
-            chainspecControllerAddresses: keys[KeyTypes.Controller].map(item => item.address),
-
-            chainspecAudiAddresses: keys[KeyTypes.Audi].map(item => item.address),
-            chainspecBabeAddresses: keys[KeyTypes.Babe].map(item => item.address),
-            chainspecGranAddresses: keys[KeyTypes.Grandpa].map(item => item.address),
-            chainspecImonAddresses: keys[KeyTypes.Imonline].map(item => item.address),
-            chainspecParaAddresses: keys[KeyTypes.Parachain].map(item => item.address),
-
-            audiSessionSeed: keys[KeyTypes.Audi][order].seed,
-            granSessionSeed: keys[KeyTypes.Grandpa][order].seed,
-            babeSessionSeed: keys[KeyTypes.Babe][order].seed,
-            imonSessionSeed: keys[KeyTypes.Imonline][order].seed,
-            paraSessionSeed: keys[KeyTypes.Parachain][order].seed,
-        };
-        const chartCfg: ChartConfig = {
-            name: 'polkadot',
-            chart: 'w3f/polkadot',
-            wait: false
-        };
-        await this.installChart(chartCfg, data);
-    }
-
-    private async installChart(chartCfg: ChartConfig, data: any = {}): Promise<void> {
-        const valuesTemplatePath = path.join(__dirname, 'values', `${chartCfg.name}.yaml`);
+        const valuesTemplatePath = path.join(__dirname, 'values', `${chartCfg.chart}.yaml`);
         const valuesTemplate = {
             path: valuesTemplatePath,
             data
         };
         chartCfg.valuesTemplate = valuesTemplate;
         if (this.dependencies &&
-            this.dependencies[chartCfg.name]) {
-            if (this.dependencies[chartCfg.name].image) {
+            this.dependencies[chartCfg.chart]) {
+            const dependencies = this.dependencies[chartCfg.chart];
+            if (dependencies.image) {
                 data['image'] = {};
                 ['repo', 'tag'].forEach((field) => {
-                    if (this.dependencies['polkadot'].image[field]) {
-                        data['image'][field] = this.dependencies['polkadot'].image[field];
+                    if (dependencies.image[field]) {
+                        data['image'][field] = dependencies.image[field];
                     }
                 });
             }
-            if (this.dependencies[chartCfg.name].chart) {
-                chartCfg.version = this.dependencies[chartCfg.name].chart;
+            if (dependencies.chart) {
+                chartCfg.version = dependencies.chart;
             }
         }
         await this.helm.install(chartCfg);
