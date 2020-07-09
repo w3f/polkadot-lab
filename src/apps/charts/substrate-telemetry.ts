c0a0e716b7f5d184484685e75f441c4a34510715
Federico Gimenez <federico.gimenez@gmail.com>
2020-07-09 13:27:07 +0200

    substrate-telemetry chart class

---
+++ src/apps/charts/substrate-telemetry.ts
@@ -0,0 +1,50 @@
+import { ChartConfig } from '@w3f/helm';
+import { Logger } from '@w3f/logger';
+
+import {
+    ChartManager,
+} from '../../types';
+import { BaseChart } from '../../helm';
+
+
+export class SubstrateTelemetryChart extends BaseChart implements ChartManager {
+    constructor(
+        protected readonly logger: Logger) {
+        super(logger);
+    }
+
+    name(): string {
+        return 'w3f/substrate-telemetry';
+    }
+    async cfg(): Promise<ChartConfig> {
+        return {
+            name: 'substrate-telemetry',
+            chart: 'w3f/substrate-telemetry',
+            wait: true
+        };
+    }
+    async values(): Promise<any> {
+        return {
+            certificate: {
+                enabled: false
+            },
+            exporter: {
+                config: {
+                    subscribe: {
+                        chains: [
+                            "Development"
+                        ]
+                    }
+                }
+            },
+            alertrules: {
+                general: {
+                    enabled: false
+                },
+                validators: {
+                    enabled: false
+                }
+            }
+        };
+    }
+}
