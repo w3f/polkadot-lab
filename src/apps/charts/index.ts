6a7c897a7a9dff8c30d32a9830622abd6ccdf6c5
Federico Gimenez <federico.gimenez@gmail.com>
2020-07-09 13:28:43 +0200

    include substrate-telemetry chart in apps exports

--- src/apps/charts/index.ts
+++ src/apps/charts/index.ts
@@ -2,3 +2,4 @@ export * from './network-policy';
 export * from './polkadot';
 export * from './polkadot-base-services';
 export * from './prometheus-operator';
+export * from './substrate-telemetry';
