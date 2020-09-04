cdf185a931fe70179a936538648e85c9b1c019a6
Federico Gimenez <federico.gimenez@gmail.com>
2020-09-04 13:26:50 +0200

    first implementation of remote platform strategy

---
+++ src/platform/remote.ts
@@ -0,0 +1,43 @@
+import { Logger } from '@w3f/logger';
+import {
+    Terraform,
+    ModuleConfig
+} from '@w3f/terraform';
+
+import { PlatformManager } from '../types';
+import { ClusterName } from '../constants';
+
+
+export class RemotePlatform implements PlatformManager {
+    private terraform: Terraform;
+    private moduleCfg: ModuleConfig;
+
+    constructor(private readonly logger: Logger) { }
+
+    async create(): Promise<void> {
+        await this.init();
+
+        return this.terraform.apply(this.moduleCfg);
+    }
+    async destroy(): Promise<void> {
+        await this.init();
+
+        return this.terraform.destroy(this.moduleCfg);
+    }
+    async getKubeconfig(): Promise<string> {
+        await this.init();
+
+        const result = await this.terraform.output(this.moduleCfg, 'kubeconfig');
+        return result as string;
+    }
+
+    private async init(): Promise<void> {
+        if (!this.terraform) {
+            this.terraform = await Terraform.create(this.logger);
+            this.moduleCfg = {
+                moduleLocation: '',
+                vars: {}
+            }
+        }
+    }
+}
